cf5be27c0f0e736eaf1db7c1918b9732427cab46
divyansh2901 <divyanshkashyap2901@gmail.com>
2026-03-26 15:13:15 +0530

    Added popNFT smart contract

---
+++ contracts/contracts/popNFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PopNFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("ProofOfParticipation", "POP") Ownable(msg.sender) {
+        tokenCounter = 0;
+    }
+
+    function mintNFT(address user, string memory metadataURI) public onlyOwner {
+        uint256 tokenId = tokenCounter;
+
+        _safeMint(user, tokenId);
+        _tokenURIs[tokenId] = metadataURI;
+
+        tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+}
